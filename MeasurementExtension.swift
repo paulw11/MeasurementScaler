ecd1b98770a58b927804a8817a52cc992e5dc062
Paul Wilkinson <paulw@wilko.me>
2019-10-01 19:08:00 +1000

    Create MeasurementExtension.swift

---
+++ MeasurementExtension.swift
@@ -0,0 +1,36 @@
+//
+//  MeasurementExtensions.swift
+//
+//  Copyright © 2019 Paul Wilkinson. 
+//
+
+
+extension Measurement where UnitType: Dimension {
+
+   
+    /// Attempt to find a  `UnitType` that produces a value less than `target`
+    /// 
+    /// See [Scaling Measurement in Swift](https://wilko.me/wordpress/?p=371)
+    /// - Parameters:
+    ///   - scales: An array of `UnitTypes` that will be applied in order seeking a value less than `target`.  This array should be in increasing order of coefficient
+    ///   - target: The target value
+    /// - Returns:
+    ///   `Self` converted to the `UnitType` that produces a value less than `target` or scaled by the last `UnitType` in the `scales` array
+
+    func scaled (scales:[UnitType], target: Double) -> Measurement {
+        guard !scales.isEmpty else {
+            return self
+        }
+        var returnMeasure = self.converted(to: scales.first!)
+        if returnMeasure.value.magnitude > target {
+            
+            for unit in scales {
+                returnMeasure.convert(to: unit)
+                if returnMeasure.value.magnitude < target {
+                    break
+                }
+            }
+        }
+        return returnMeasure
+    }
+}
